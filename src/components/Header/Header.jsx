e8845837731496badab9f168e0da4c5487e8cbd8
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2023-09-07 23:55:48 +0200

    🚧 HEADER > Layout > Add navbar layout

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,41 @@
+import { styled } from "styled-components";
+
+export default function Header() {
+  return (
+    <HeaderStyled>
+      <div className="navbar">
+        <div className="Logo">logo</div>
+        <nav>
+          <p>Links</p>
+          <p>Links</p>
+          <p>Links</p>
+        </nav>
+      </div>
+      <div className="banner">banner</div>
+    </HeaderStyled>
+  );
+}
+
+const HeaderStyled = styled.div`
+  .navbar {
+    background-color: red;
+    height: 10vh;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+
+    .Logo {
+      background-color: Pink;
+    }
+
+    nav {
+      background-color: purple;
+      display: flex;
+
+      p {
+        padding: 5px;
+      }
+    }
+  }
+`;
