60aaaae8ebb9ce1065e4e8053812c3dec7ea1331
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2023-09-08 23:30:20 +0200

    ♻️ HEADER > Navbar > Create Links component

---
+++ src/components/Header/Links.jsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import { styled } from "styled-components";
+
+export default function Links() {
+  return (
+    <LinksStyled>
+      <Link className="link" to="/">
+        Home
+      </Link>
+      <Link className="link" to="/">
+        Services
+      </Link>
+      <Link className="link" to="/">
+        Price
+      </Link>
+    </LinksStyled>
+  );
+}
+
+const LinksStyled = styled.nav`
+  display: flex;
+
+  .link {
+    color: #18272f;
+    position: relative;
+    text-decoration: none;
+    margin: 5px;
+
+    &::before {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 2px;
+      border-radius: 4px;
+      background-color: #1bbabf;
+      bottom: 0;
+      left: 0;
+      transform-origin: right;
+      transform: scaleX(0);
+      transition: transform 0.3s ease-in-out;
+    }
+    &:hover::before {
+      transform-origin: left;
+      transform: scaleX(1);
+    }
+    &:hover {
+      color: #1bbabf;
+    }
+  }
+`;
--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,22 +1,12 @@
-import { Link } from "react-router-dom";
 import Logo from "../../assets/images/CMG-Horizontal-Logo-RGB-Full-Color.png";
 import { styled } from "styled-components";
+import Links from "./Links";
 
 export default function Navbar() {
   return (
     <NavbarStyled className="navbar">
       <img className="Logo" src={Logo} alt="" />
-      <nav>
-        <Link className="link" to="/">
-          Home
-        </Link>
-        <Link className="link" to="/">
-          Services
-        </Link>
-        <Link className="link" to="/">
-          Price
-        </Link>
-      </nav>
+      <Links />
     </NavbarStyled>
   );
 }
@@ -30,37 +20,5 @@ const NavbarStyled = styled.div`
 
   .Logo {
     height: 50px;
-  }
-
-  nav {
-    display: flex;
-
-    .link {
-      color: #18272f;
-      position: relative;
-      text-decoration: none;
-      margin: 5px;
-
-      &::before {
-        content: "";
-        position: absolute;
-        width: 100%;
-        height: 2px;
-        border-radius: 4px;
-        background-color: #1bbabf;
-        bottom: 0;
-        left: 0;
-        transform-origin: right;
-        transform: scaleX(0);
-        transition: transform 0.3s ease-in-out;
-      }
-      &:hover::before {
-        transform-origin: left;
-        transform: scaleX(1);
-      }
-      &:hover {
-        color: #1bbabf;
-      }
-    }
   }
 `;
