8f9e79384afacfa4bc29a90db0feca1a333f5f38
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2023-09-09 00:12:18 +0200

    ♻️ HEADER > Banner > Create Banner component

---
+++ src/components/Header/Banner.jsx
@@ -0,0 +1,60 @@
+import { styled } from "styled-components";
+
+export default function Banner() {
+  return (
+    <BannerStyled>
+      <h1>Complete it services</h1>
+      <h3>for Residential, Small & Medium Businesses</h3>
+
+      <div className="cta">
+        <button>Learn more</button>
+        <button>Contact</button>
+      </div>
+    </BannerStyled>
+  );
+}
+
+const BannerStyled = styled.div`
+    background-color: #1bbabf;
+    height: 90vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+
+    h1,
+    h3 {
+      color: white;
+      font-family: "Montserrat", sans-serif;
+      font-weight: 700;
+      text-transform: uppercase;
+    }
+    h1 {
+      font-size: 60px;
+    }
+    h3 {
+      font-size: 20px;
+    }
+
+    .cta {
+
+      button {
+        width: 140px;
+        height: 50px;
+        color: #1bbabf;
+        background-color: white;
+        border: 1px solid white;
+        border-radius: 40px;
+        margin: 10px;
+        font-family: 'Hind', sans-serif;
+
+        &:hover {
+          cursor: pointer;
+          color: white;
+          background-color: #1bbabf;
+          transition: all 0.5s ease-in-out;
+        }
+      }
+    }
+  `;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,66 +1,11 @@
-import { styled } from "styled-components";
 import Navbar from "./Navbar";
+import Banner from "./Banner";
 
 export default function Header() {
   return (
-    <HeaderStyled>
+    <>
       <Navbar />
-      <div className="banner">
-        <h1>Complete it services</h1>
-        <h3>for Residential, Small & Medium Businesses</h3>
-
-        <div className="cta">
-          <button>Learn more</button>
-          <button>Contact</button>
-        </div>
-      </div>
-    </HeaderStyled>
+      <Banner />
+    </>
   );
 }
-
-const HeaderStyled = styled.div`
-  .banner {
-    background-color: #1bbabf;
-    height: 90vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-
-    h1,
-    h3 {
-      color: white;
-      font-family: "Montserrat", sans-serif;
-      font-weight: 700;
-      text-transform: uppercase;
-    }
-    h1 {
-      font-size: 60px;
-    }
-    h3 {
-      font-size: 20px;
-    }
-
-    .cta {
-
-      button {
-        width: 140px;
-        height: 50px;
-        color: #1bbabf;
-        background-color: white;
-        border: 1px solid white;
-        border-radius: 40px;
-        margin: 10px;
-        font-family: 'Hind', sans-serif;
-
-        &:hover {
-          cursor: pointer;
-          color: white;
-          background-color: #1bbabf;
-          transition: all 0.5s ease-in-out;
-        }
-      }
-    }
-  }
-`;
