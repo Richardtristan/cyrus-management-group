58abd63b3774a6d371a7a7f410445407fa450fd8
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2023-09-08 23:13:43 +0200

    💄 HEADER > Navbar > Add link style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,14 +7,14 @@ export default function Header() {
       <div className="navbar">
         <div className="Logo">logo</div>
         <nav>
-          <Link to="/">
-            <p>Home</p>
+          <Link className="link" to="/">
+            Home
           </Link>
-          <Link to="/">
-            <p>Services</p>
+          <Link className="link" to="/">
+            Services
           </Link>
-          <Link to="/">
-            <p>Price</p>
+          <Link className="link" to="/">
+            Price
           </Link>
         </nav>
       </div>
@@ -38,14 +38,31 @@ const HeaderStyled = styled.div`
     nav {
       display: flex;
 
-      p {
+      .link {
+        color: #18272f;
+        position: relative;
         text-decoration: none;
         margin: 5px;
-        color: Black;
 
+        &::before {
+          content: "";
+          position: absolute;
+          width: 100%;
+          height: 2px;
+          border-radius: 4px;
+          background-color: #1bbabf;
+          bottom: 0;
+          left: 0;
+          transform-origin: right;
+          transform: scaleX(0);
+          transition: transform 0.3s ease-in-out;
+        }
+        &:hover::before {
+          transform-origin: left;
+          transform: scaleX(1);
+        }
         &:hover {
-            cursor: pointer;
-            color: white;
+            color: #1bbabf;
         }
       }
     }
