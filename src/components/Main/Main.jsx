95bd08740f7ab334d81152431a5b7701ff681ae2
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2023-09-10 00:36:30 +0200

    ♻️ MAIN > Create <Gradient />

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,11 @@
 import { styled } from "styled-components";
 import Services from "./Services";
+import Gradient from "../reusable-ui/Gradient";
 
 export default function Main() {
   return (
     <MainStyled>
-      <div className="gradient"></div>
+      <Gradient />
       <Services />
     </MainStyled>
   );
@@ -16,18 +17,4 @@ const MainStyled = styled.div`
   align-items: center;
   justify-content: center;
   position: relative;
-  .gradient {
-    height: 1100px;
-    width: 100vw;
-    flex-shrink: 0;
-    background: linear-gradient(
-      180deg,
-      #1bbabf 8.28%,
-      rgba(255, 255, 255, 0) 50%
-    );
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: -1;
-  }
 `;
---
+++ src/components/reusable-ui/Gradient.jsx
@@ -0,0 +1,20 @@
+import { styled } from "styled-components";
+
+export default function Gradient({ className }) {
+  return <GradientStyled className={className}></GradientStyled>;
+}
+
+const GradientStyled = styled.div`
+  height: 1100px;
+  width: 100vw;
+  flex-shrink: 0;
+  background: linear-gradient(
+    180deg,
+    #1bbabf 8.28%,
+    rgba(255, 255, 255, 0) 50%
+  );
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+`;
