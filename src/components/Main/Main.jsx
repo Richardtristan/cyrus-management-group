f6d22a84bc3b5bc59c69837028c4408419744af8
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2023-09-10 00:48:58 +0200

    💄 MAIN > add cta

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,18 @@
 import { styled } from "styled-components";
 import Services from "./Services";
 import Gradient from "../reusable-ui/Gradient";
+import SecondaryButton from "../reusable-ui/SecondaryButton";
 
 export default function Main() {
   return (
     <MainStyled>
       <Gradient />
       <Services />
+      <div className="cta">
+        <h3>Discover how I can assist you in realizing your goals. </h3>
+
+        <SecondaryButton label={"Contact"} />
+      </div>
     </MainStyled>
   );
 }
@@ -17,4 +23,18 @@ const MainStyled = styled.div`
   align-items: center;
   justify-content: center;
   position: relative;
+
+  .cta {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 120px;
+    h3 {
+      text-align: center;
+      font-family: "Montserrat", sans-serif;
+      font-size: 18px;
+      font-weight: 900;
+      margin-bottom: 20px;
+    }
+  }
 `;
