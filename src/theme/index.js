01ffeef5bf149f0751334d4008a9fc9d5b44060a
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2023-09-10 00:58:27 +0200

    💄 THEME > Create design system

---
+++ src/theme/index.js
@@ -0,0 +1,52 @@
+const colors = {
+  dark: "#17161a",
+  white: "white",
+  Blue: "#1bbabf",
+};
+
+const spacing = {
+  xxs: "4px",
+  xs: "8px",
+  sm: "12px",
+  md: "20px",
+  lg: "32px",
+  xl: "52px",
+  xxl: "84px",
+};
+
+const fonts = {
+  size: {
+    XXXS: "8px",
+    XXS: "10px",
+    XS: "12px",
+    P0: "15px",
+    P1: "18px",
+    P2: "20px",
+    P3: "24px",
+    P4: "36px",
+    P5: "48px",
+    P6: "60px",
+  },
+  weights: {
+    light: 300,
+    regular: 400,
+    medium: 500,
+    semiBold: 600,
+    bold: 700,
+    heavy: 800,
+  },
+};
+const borderRadius = {
+  round: "10px",
+  extraRound: "40px",
+  circle: "50%",
+};
+const shadows = {};
+
+export const theme = {
+  colors,
+  fonts,
+  borderRadius,
+  shadows,
+  spacing,
+};
