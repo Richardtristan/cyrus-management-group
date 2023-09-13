11c6cd894fe1128a31d91262bb3dc5f70229f397
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2023-09-13 14:59:03 +0200

    ♻️ MAIN > Create <Price />

---
+++ src/components/Main/Price.jsx
@@ -0,0 +1,36 @@
+import { styled } from "styled-components";
+import CardPrice from "../reusable-ui/CardPrice";
+import CardPriceHourly from "./CardPriceHourly";
+
+export default function Price() {
+  return (
+    <PriceStyled>
+      <h2>Price</h2>
+      <div className="cards">
+        <CardPrice price={140} title={"Montly Basic"} />
+        <CardPrice price={840} title={"Yearly Basic"} />
+      </div>
+      <CardPriceHourly />
+    </PriceStyled>
+  );
+}
+
+const PriceStyled = styled.div`
+  h2 {
+    text-align: center;
+    text-transform: uppercase;
+    color: #000;
+    text-align: center;
+    font-family: "Montserrat", sans-serif;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 900;
+    line-height: normal;
+    margin: 30px 0;
+  }
+  .cards {
+    display: flex;
+    gap: 50px;
+    flex-wrap: wrap;
+  }
+`;
