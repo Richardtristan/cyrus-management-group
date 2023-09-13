4923e79064040bdd6f73e94681f9e9817a70d094
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2023-09-13 14:59:35 +0200

    ✨ MAIN > Pice > Add Price Components

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,18 +1,16 @@
 import { styled } from "styled-components";
 import Services from "./Services";
 import Gradient from "../reusable-ui/Gradient";
-import SecondaryButton from "../reusable-ui/SecondaryButton";
+import Cta from "./Cta";
+import Price from "./Price";
 
 export default function Main() {
   return (
     <MainStyled>
       <Gradient />
       <Services />
-      <div className="cta">
-        <h3>Discover how I can assist you in realizing your goals. </h3>
-
-        <SecondaryButton label={"Contact"} />
-      </div>
+      <Cta />
+      <Price />
     </MainStyled>
   );
 }
@@ -23,18 +21,4 @@ const MainStyled = styled.div`
   align-items: center;
   justify-content: center;
   position: relative;
-
-  .cta {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 120px;
-    h3 {
-      text-align: center;
-      font-family: "Montserrat", sans-serif;
-      font-size: 18px;
-      font-weight: 900;
-      margin-bottom: 20px;
-    }
-  }
 `;
