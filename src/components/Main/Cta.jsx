748a2f68db0c73193f554282cd7bdb3b839de27e
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2023-09-13 15:07:18 +0200

    💄 MAIN > Cta > Add background-color

--- src/components/Main/Cta.jsx
+++ src/components/Main/Cta.jsx
@@ -5,7 +5,6 @@ export default function Cta() {
   return (
     <CtaStyled>
     <h3>Discover how I can assist you in realizing your goals. </h3>
-
     <SecondaryButton label={"Contact"} />
   </CtaStyled>
   )
@@ -16,11 +15,15 @@ const CtaStyled = styled.div`
     flex-direction: column;
     align-items: center;
     margin-top: 120px;
+    background-color: #1bbabf;
+    width: 100%;
+    padding: 50px 0px;
     h3 {
       text-align: center;
       font-family: "Montserrat", sans-serif;
       font-size: 18px;
       font-weight: 900;
       margin-bottom: 20px;
+      color: White;
     }
 `;
